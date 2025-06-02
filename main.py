1d77a10c6ff8af5c42becc2cf460d35dcf964666
dsw56089 <dsw56089@student.dsw.edu.pl>
2025-06-02 14:20:05 +0200

    Task2: JSON

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # main.py
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filepath")
 args = parser.parse_args()
 print(f"Plik wejściowy: {args.filepath}")
+with open(args.filepath, 'r') as f:
+    try:
+        data = json.load(f)
+        print("Poprawnie wczytano JSON.")
+    except json.JSONDecodeError as e:
+        print(f"zła składnia JSON: {e}")
