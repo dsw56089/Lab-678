86f1454b900baf40145aa563bc0242e9e35cab5f
dsw56089 <dsw56089@student.dsw.edu.pl>
2025-06-02 14:30:13 +0200

    Task3: zapis danych JSON

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("filepath")
 args = parser.parse_args()
 print(f"Plik wejściowy: {args.filepath}")
-with open(args.filepath, 'r') as f:
-    try:
+
+try:
+    with open(args.filepath, 'r') as f:
         data = json.load(f)
-        print("Poprawnie wczytano JSON.")
-    except json.JSONDecodeError as e:
-        print(f"zła składnia JSON: {e}")
+        print("wczytano JSON.")
+except json.JSONDecodeError as e:
+    print(f"Błąd: {e}")
+    exit(1)
+except FileNotFoundError:
+    print("Plik nie został znaleziony.")
+    exit(1)
+with open('output.json', 'w') as f:
+    json.dump(data, f, indent=4)
+    print("Zapisano dane do output.json")
