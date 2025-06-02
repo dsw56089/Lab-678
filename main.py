41d85b6af38988c4ef5f9668bf1e6d280ec03739
dsw56089 <dsw56089@student.dsw.edu.pl>
2025-06-02 15:28:08 +0200

    Task6: wczytywanie XML

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import json
 import yaml
 import os
-
+from lxml.etree import ElementTree as ET, parse, tostring, Element
 parser = argparse.ArgumentParser()
 parser.add_argument("filepath")
 parser.add_argument("output")
@@ -31,6 +31,15 @@ try:
             except yaml.YAMLError as e:
                 print(f"Błąd składni YAML: {e}")
                 exit(1)
+        elif extension == ".xml":
+            try:
+                tree = parse(f)
+                root = tree.getroot()
+                data = {child.tag: child.text for child in root}
+                print("Poprawnie wczytano XML.")
+            except Exception as e:
+                print(f"Błąd składni XML: {e}")
+                exit(1)
         else:
             print("Nieobsługiwane rozszerzenie pliku. Użyj .json, .yaml lub .yml.")
             exit(1)
@@ -46,6 +55,19 @@ try:
         with open(args.output, 'w') as f:
             json.dump(data, f, indent=4)
             print("Zapisano dane" + args.output)
+    elif extension_output == ".xml":
+        try:
+            root = Element("root")
+            for key, value in data.items():
+                child = Element(key)
+                child.text = value
+                root.append(child)
+            with open(args.output, 'wb') as f:
+                f.write(tostring(root, pretty_print=True, xml_declaration=True, encoding="UTF-8"))
+            print("Zapisano dane " + args.output)
+        except Exception as e:
+            print(f"Błąd podczas zapisu XML: {e}")
+            exit(1)
 except Exception as e:
     print(f"Błąd podczas zapisu pliku: {e}")
     exit(1)
